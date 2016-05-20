19f1e1e011871c2052cff8e49bd0ef764d465a8d
bilel-kedidi <bilel.kedidi@gmail.com>
2016-05-20 14:12:25 +0100

    fix styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,29 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require "front_end/jquery-1.6.3.min.js" 
-//= require "front_end/cufon-yui.js"
-//= require "front_end/cufon-replace.js"
-//= require "front_end/NewsGoth_400.font.js"
-//= require "front_end/NewsGoth_700.font.js"
-//= require "front_end/NewsGoth_Lt_BT_italic_400.font.js"
-//= require "front_end/Vegur_400.font.js"
-//= require "front_end/FF-cash.js"
-//= require "front_end/jquery.featureCarousel.js"
-    $(document).ready(function() {
-        $("#carousel").featureCarousel({
-            autoPlay:7000,
-            trackerIndividual:false,
-            trackerSummation:false,
-            topPadding:50,
-            smallFeatureWidth:.9,
-            smallFeatureHeight:.9,
-            sidePadding:0,
-            smallFeatureOffset:0
-        });
-    });
-
-
-$(document).ready(function(){
-    $('form').addClass("form-horizontal")
-})
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap.min
